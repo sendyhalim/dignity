6713f217b095b7dc7073b0ff01829db88293affa
sendyHalim <sendy_ubm@yahoo.com>
2014-10-21 23:29:22 +0700

    Add tests

---
+++ test/dummy/user.js
@@ -0,0 +1,11 @@
+module.exports = {
+  permissions: [
+    { name: 'Googling' },
+    { name: 'Playing' },
+    { name: 'watCHing' }
+  ],
+  roles: [
+    { name: 'editor' },
+    { name: 'ironmAn' }
+  ]
+};
---
+++ test/layers/bookshelf-layer.js
@@ -0,0 +1,51 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var BookshelfLayer = require('../../lib/layers/bookshelf-layer');
+var userDummy = require('../dummy/user');
+var rolesDummy = require('../dummy/roles');
+var permissionsDummy = require('../dummy/permissions');
+
+
+describe('Bookshelf Layer', function () {
+  var layer = undefined;
+  var user = {
+    load: function (){}
+  };
+
+  before(function (done) {
+    sinon.stub(user, 'load').returns({
+      then: function (callback){
+        callback(userDummy);
+      }
+    });
+
+    done();
+  });
+
+  beforeEach(function (done) {
+    layer = new BookshelfLayer(user);
+    done();
+  });
+
+  it('should have permissions as an array', function () {
+    expect(layer.getPermissions()).to.be.an('array');
+  });
+
+  it('should have roles as an array', function () {
+    expect(layer.getRoles()).to.be.an('array');
+  });
+
+  it('should check user roles', function () {
+    expect(layer.is(rolesDummy)).to.equal(true);
+    expect(layer.hasRole(rolesDummy)).to.equal(true);
+    expect(layer.is('nobody')).to.equal(false);
+    expect(layer.hasRole('nobody')).to.equal(false);
+  });
+
+  it('should check user permissions', function () {
+    expect(layer.can(permissionsDummy)).to.equal(true);
+    expect(layer.hasPermission(permissionsDummy)).to.equal(true);
+    expect(layer.can('sing-all-day-long')).to.equal(false);
+    expect(layer.hasPermission('sing-all-day-long')).to.equal(false);
+  });
+});
