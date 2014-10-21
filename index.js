d6aca9c0ed76076ff768264cda9c41c33a4217ee
sendyHalim <sendy_ubm@yahoo.com>
2014-10-22 01:30:53 +0700

    replace analyzeDignity() with direct constructor

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 var BookshelfLayer = require('./lib/layers/bookshelf-layer');
 
-
-var analyzeDignity = function analyzeDignity (user, orm) {
-  orm = orm.toLowerCase();
-
-  switch (orm) {
-    case 'bookshelf': return new BookshelfLayer(user);
-                      break;
-
-    default: throw new Error('Layer is not supported');
-  }
-};
-
 module.exports = {
-  analyzeDignity: analyzeDignity
+  BookshelfLayer: BookshelfLayer
 };
