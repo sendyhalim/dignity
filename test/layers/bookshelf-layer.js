26cb1696c8697a6d99ee03af72fc61f214923193
sendyHalim <sendy_ubm@yahoo.com>
2014-10-25 23:11:32 +0700

    Use different knex instance for migration to enable flexible dbname

--- test/layers/bookshelf-layer.js
+++ test/layers/bookshelf-layer.js
@@ -4,6 +4,7 @@ var BookshelfLayer = require('../../lib/layers/bookshelf-layer');
 var rolesDummy = require('../dummy/roles');
 var permissionsDummy = require('../dummy/permissions');
 var models = require('../models');
+var fs = require('fs');
 
 describe('Bookshelf Layer', function () {
   this.timeout(15000);
@@ -11,7 +12,7 @@ describe('Bookshelf Layer', function () {
   var user;
 
   before(function (done) {
-    models.prepareDummyData().then(function (r) {
+    models.prepareAllDummyData(models.bookshelfDatabase).then(function (r) {
       (new models.bookshelf.User({
         id: 1
       }))
@@ -24,10 +25,8 @@ describe('Bookshelf Layer', function () {
   });
 
   after(function (done) {
-    var fs = require('fs');
-    // delete sqlite file
-    fs.unlinkSync(__dirname + '/../../bookshelfjs-test.sqlite');
-    done()
+    fs.unlinkSync(models.bookshelfDatabase);
+    done();
   });
 
   // create new layer and analyze user's roles and permissions for each test
@@ -38,27 +37,31 @@ describe('Bookshelf Layer', function () {
     });
   });
 
-  it('should have permissions as an array', function () {
+  it('should have permissions as an array', function (done) {
     expect(layer.getPermissions()).to.be.an('array');
+    done();
   });
 
-  it('should have roles as an array', function () {
+  it('should have roles as an array', function (done) {
     expect(layer.getRoles()).to.be.an('array');
+    done()
   });
 
-  it('should check user roles', function () {
+  it('should check user roles', function (done) {
     expect(layer.is(rolesDummy)).to.equal(true);
     expect(layer.hasRole(rolesDummy)).to.equal(true);
     expect(layer.is('nobody')).to.equal(false);
     expect(layer.hasRole('nobody')).to.equal(false);
+    done()
   });
 
-  it('should check user permissions', function () {
+  it('should check user permissions', function (done) {
     expect(layer.can(permissionsDummy)).to.equal(true);
     expect(layer.hasPermission(permissionsDummy)).to.equal(true);
     expect(layer.can('sing-all-day-long')).to.equal(false);
     expect(layer.hasPermission('sing-all-day-long')).to.equal(false);
 
     expect(layer.getPermissions().length).to.equal(3);
+    done();
   });
 });
--- test/models/index.js
+++ test/models/index.js
@@ -1,9 +1,10 @@
-var bookshelf = require('../config/bookshelf');
+
 
 // ----------------------------------------------------
 // For inserting dummy data
 // ----------------------------------------------------
 var bookshelfModels = require('./bookshelf-models');
+var sequelizeModels = require('./sequelize-models');
 var User = bookshelfModels.User;
 var Permission = bookshelfModels.Permission;
 var Role = bookshelfModels.Role;
@@ -12,124 +13,137 @@ var dummyUser;
 var dummyRoles;
 var dummyPermissions;
 
-var createTables = function () {
-  var tables = [];
-
-  tables.push(bookshelf.knex.schema.createTable('users', function (t) {
-    t.increments();
-    t.string('name');
-  }));
-
-  tables.push(bookshelf.knex.schema.createTable('roles', function (t) {
-    t.increments();
-    t.string('name');
-  }));
-
-  tables.push(bookshelf.knex.schema.createTable('permissions', function (t) {
-    t.increments();
-    t.string('name');
-    t.string('display_name');
-  }));
-
-  tables.push(bookshelf.knex.schema.createTable('users_roles', function (t) {
-    t.increments();
-    t.integer('role_id').unsigned();
-    t.integer('user_id').unsigned();
-  }));
-
-  tables.push(bookshelf.knex.schema.createTable('roles_permissions', function (t) {
-    t.increments();
-    t.integer('role_id').unsigned();
-    t.integer('permission_id').unsigned();
-  }));
-
-  return Promise.all(tables);
-};
-
-var insertModels = function () {
-  var models = [];
-
-  // ----------------------------------------------------
-  // Creating Users
-  // ----------------------------------------------------
-  dummyUser = {
-    id: 1,
-    name: 'Monyet Sahur'
-  };
-
-  models.push(bookshelf.knex.insert(dummyUser).into('users'));
-
-
-  // ----------------------------------------------------
-  // Creating roles
-  // ----------------------------------------------------
-  dummyRoles = {
-    ironman: {
-      id: 1,
-      name: 'ironman'
-    },
-    editor: {
-      id: 2,
-      name: 'editor'
+var prepareAllDummyData = function (dbName) {
+  // create db based on given dbName
+  var dbConfig = {
+    client: 'sqlite3',
+    connection: {
+      filename: dbName
     }
   };
 
-  models.push(bookshelf.knex.insert(dummyRoles.ironman).into('roles'));
-  models.push(bookshelf.knex.insert(dummyRoles.editor).into('roles'));
+  var knex = require('knex')(dbConfig);
 
-  // ----------------------------------------------------
-  // Creating permissions
-  // ----------------------------------------------------
-  dummyPermissions = {
-    playing: {
+  var createTables = function () {
+    var tables = [];
+
+    tables.push(knex.schema.createTable('users', function (t) {
+      t.increments();
+      t.string('name');
+    }));
+
+    tables.push(knex.schema.createTable('roles', function (t) {
+      t.increments();
+      t.string('name');
+    }));
+
+    tables.push(knex.schema.createTable('permissions', function (t) {
+      t.increments();
+      t.string('name');
+      t.string('display_name');
+    }));
+
+    tables.push(knex.schema.createTable('users_roles', function (t) {
+      t.increments();
+      t.integer('role_id').unsigned();
+      t.integer('user_id').unsigned();
+    }));
+
+    tables.push(knex.schema.createTable('roles_permissions', function (t) {
+      t.increments();
+      t.integer('role_id').unsigned();
+      t.integer('permission_id').unsigned();
+    }));
+
+    return Promise.all(tables);
+  }; // end createTables()
+
+  var insertModels = function () {
+    var models = [];
+
+    // ----------------------------------------------------
+    // Creating Users
+    // ----------------------------------------------------
+    dummyUser = {
       id: 1,
-      name: 'PlayIng',
-      display_name: 'PlayIng'
-    },
-    googling: {
-      id: 2,
-      name: 'GoogLING',
-      display_name: 'GoogLING'
-    },
-    watching: {
-      id: 3,
-      name: 'WatcHing',
-      display_name: 'WatcHing'
-    }
-  };
+      name: 'Monyet Sahur'
+    };
 
-  models.push(bookshelf.knex.insert(dummyPermissions.playing).into('permissions'));
-  models.push(bookshelf.knex.insert(dummyPermissions.googling).into('permissions'));
-  models.push(bookshelf.knex.insert(dummyPermissions.watching).into('permissions'));
+    models.push(knex.insert(dummyUser).into('users'));
 
-  return Promise.all(models);
-};// end insertData()
 
-var insertRolesAndPermissions = function () {
-  var rolesAndPermissions = [];
+    // ----------------------------------------------------
+    // Creating roles
+    // ----------------------------------------------------
+    dummyRoles = {
+      ironman: {
+        id: 1,
+        name: 'ironman'
+      },
+      editor: {
+        id: 2,
+        name: 'editor'
+      }
+    };
 
-  rolesAndPermissions.push(bookshelf.knex.insert([
-    { id: 1, user_id: dummyUser.id, role_id: dummyRoles.ironman.id },
-    { id: 2, user_id: dummyUser.id, role_id: dummyRoles.editor.id },
-  ])
-  .into('users_roles'));
+    models.push(knex.insert(dummyRoles.ironman).into('roles'));
+    models.push(knex.insert(dummyRoles.editor).into('roles'));
 
-  rolesAndPermissions.push(bookshelf.knex.insert([
-    { id: 1, role_id: dummyRoles.editor.id, permission_id: dummyPermissions.googling.id},
-    { id: 2, role_id: dummyRoles.editor.id, permission_id: dummyPermissions.watching.id},
+    // ----------------------------------------------------
+    // Creating permissions
+    // ----------------------------------------------------
+    dummyPermissions = {
+      playing: {
+        id: 1,
+        name: 'PlayIng',
+        display_name: 'PlayIng'
+      },
+      googling: {
+        id: 2,
+        name: 'GoogLING',
+        display_name: 'GoogLING'
+      },
+      watching: {
+        id: 3,
+        name: 'WatcHing',
+        display_name: 'WatcHing'
+      }
+    };
 
-    { id: 3, role_id: dummyRoles.ironman.id, permission_id: dummyPermissions.playing.id},
-    { id: 4, role_id: dummyRoles.ironman.id, permission_id: dummyPermissions.watching.id},
-  ]).into('roles_permissions'));
+    models.push(knex.insert(dummyPermissions.playing).into('permissions'));
+    models.push(knex.insert(dummyPermissions.googling).into('permissions'));
+    models.push(knex.insert(dummyPermissions.watching).into('permissions'));
 
-  return Promise.all(rolesAndPermissions);
-};
+    return Promise.all(models);
+  };// end insertModels()
 
-var prepareDummyData = function () {
+  var insertRolesAndPermissions = function () {
+    var rolesAndPermissions = [];
+
+    rolesAndPermissions.push(knex.insert([
+      { id: 1, user_id: dummyUser.id, role_id: dummyRoles.ironman.id },
+      { id: 2, user_id: dummyUser.id, role_id: dummyRoles.editor.id },
+    ])
+    .into('users_roles'));
+
+    rolesAndPermissions.push(knex.insert([
+      { id: 1, role_id: dummyRoles.editor.id, permission_id: dummyPermissions.googling.id},
+      { id: 2, role_id: dummyRoles.editor.id, permission_id: dummyPermissions.watching.id},
+
+      { id: 3, role_id: dummyRoles.ironman.id, permission_id: dummyPermissions.playing.id},
+      { id: 4, role_id: dummyRoles.ironman.id, permission_id: dummyPermissions.watching.id},
+    ]).into('roles_permissions'));
+
+    return Promise.all(rolesAndPermissions);
+  };// end insertRolesAndPermissions()
+
   return Promise.all([createTables(), insertModels(), insertRolesAndPermissions()]);
 };
 
 module.exports = {
-  prepareDummyData: prepareDummyData,
-  bookshelf: bookshelfModels
+  prepareAllDummyData: prepareAllDummyData,
+  bookshelf: bookshelfModels,
+  sequelize: sequelizeModels,
+  bookshelfDatabase: './bookshelf-test.sqlite',
+  sequelizeDatabase: './sequelize-test.sqlite'
 }
